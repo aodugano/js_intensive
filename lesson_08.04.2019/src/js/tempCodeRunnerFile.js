33e1b4208d4cdf3d61aae0e474209357a6d1aeeb
Александр Дуганов <44617790+aodugano@users.noreply.github.com>
2019-04-08 21:23:38 +0300

    Second intensive of JS
    The first day of the intensive by JS (our yt)

---
+++ lesson_08.04.2019/src/js/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const switcher = document.querySelector('#cbx'),
+      more = document.querySelector('.more'),
+      mpdal = document.querySelector('.mpdal'),
+      videos = document.querySelector('.videos__item');
